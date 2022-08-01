d1bbcc83d136769228e89bd095c816dddc960804
GitHub Action <action@github.com>
2022-08-01 17:55:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/dominiklippl/jetmoco/MyBundle.kt
+++ src/main/kotlin/com/github/dominiklippl/jetmoco/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dominiklippl.jetmoco
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dominiklippl/jetmoco/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dominiklippl/jetmoco/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dominiklippl.jetmoco.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dominiklippl.jetmoco.MyBundle
 
 class MyApplicationService {
 
